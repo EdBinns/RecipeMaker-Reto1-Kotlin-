29385bb66c57027049df3ee2398ab912b68e0cc8
EduardoBinns <57016116+EduardoBinns@users.noreply.github.com>
2020-04-09 21:17:54 -0600

    Segundo reto kotlin

--- src/Ingredientes.kt
+++ src/Ingredientes.kt
@@ -12,6 +12,7 @@ fun main(args : Array<String>){
 
 fun showMenu(){
     val menu: String = """
+        _________________________________________________
         Bienvenido a recipe maker
             1.Hacer una receta
             2.Ver mis recetas
@@ -24,9 +25,11 @@ fun showMenu(){
         1 ->  {makeRecipe()
                showMenu()
         }
-        2 -> { for ((index,recipe)  in listRecipes.withIndex()){ println("Esta viendo la receta  $index  ${recipe.nombreReceta}") }
-               showMenu()
+        2 -> {
+            viewRecipe()
+            showMenu()
         }
+
         3 -> println("Muchas gracias por su visita")
         else -> {
             println("El elemento ingresado no es una opción")
@@ -35,7 +38,17 @@ fun showMenu(){
     }
 
 }
-
+fun viewRecipe() {
+    val paragraphViewReciper = """
+        _________________________________________________
+        Ver mis recetas
+        
+    """.trimIndent()
+    val showRecipe= {
+        println(paragraphViewReciper)
+        for ((index, recipe) in listRecipes.withIndex()) { println("Esta viendo la receta  ${index + 1}  ${recipe.nombreReceta}")}}
+    showRecipe()
+}
 fun makeRecipe() {
     val list: MutableList<Ingredientes> = ArrayList()
 
@@ -43,9 +56,32 @@ fun makeRecipe() {
     val nameRecipe: String? = readLine()
     var flag: Boolean = true
     while (flag) {
-        println("Ingrese el nombre del ingrediente")
-        val nameIngredient: String? = readLine()
-        list.add(Ingredientes(nameIngredient))
+        val namesingredients = """
+            _________________________________________________
+            La siguiente lista son los posibles ingredientes que podra tener su receta: 
+                1.Agua
+                2.Leche
+                3.Carne
+                4.Verduras
+                5.Frutas
+                6.Cereal
+                7.Huevos
+                8.Aceite
+        """.trimIndent()
+        println(namesingredients)
+        println("Ingrese el numero correspondiente al ingrediente que desea ingresar a la receta: ")
+        val nameIngredient: Int? = readLine()?.toInt()
+        when(nameIngredient){
+            1->list.add(Ingredientes("Agua"))
+            2->list.add(Ingredientes("Leche"))
+            3->list.add(Ingredientes("Carne"))
+            4->list.add(Ingredientes("Verduras"))
+            5->list.add(Ingredientes("Frutas"))
+            6->list.add(Ingredientes("Cereal"))
+            7->list.add(Ingredientes("Huevos"))
+            8->list.add(Ingredientes("Aceite"))
+            else-> println("Ingreso un valor invalido, por favor intentelo de nuevo")
+        }
         println("Desea agregar otro ingrediente?(Si/No)")
         val askNextIngredient: String? = readLine()
         flag = askNextIngredient?.capitalize().equals("Si")
